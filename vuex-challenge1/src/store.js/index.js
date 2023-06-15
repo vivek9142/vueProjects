8667c9dd22d693befc3b692db6c5b777aeaf3882
Vivek <vivek9142@gmail.com>
2023-06-15 13:03:27 +0530

    init commit

---
+++ vuex-challenge1/src/store.js/index.js
@@ -0,0 +1,58 @@
+import { createStore } from 'vuex';
+import initialState from './initialState';
+
+const store = createStore({
+  state: initialState,
+  mutations: {
+    addProductToCart(state, productData) {
+      const productInCartIndex = state.cart.items.findIndex(
+        (ci) => ci.productId === productData.id
+      );
+
+      if (productInCartIndex >= 0) {
+        state.cart.items[productInCartIndex].qty++;
+      } else {
+        const newItem = {
+          productId: productData.id,
+          title: productData.title,
+          image: productData.image,
+          price: productData.price,
+          qty: 1,
+        };
+        state.cart.items.push(newItem);
+      }
+      state.cart.qty++;
+      state.cart.total += productData.price;
+    },
+
+    removeProductFromCart(state, prodId) {
+      const productInCartIndex = state.cart.items.findIndex(
+        (cartItem) => cartItem.productId === prodId
+      );
+      const prodData = state.cart.items[productInCartIndex];
+      state.cart.items.splice(productInCartIndex, 1);
+      state.cart.qty -= prodData.qty;
+
+      state.cart.total -= prodData.price * prodData.qty;
+    },
+    login(state) {
+      state.isLoggedIn = true;
+    },
+    logout(state) {
+      state.isLoggedIn = false;
+    },
+  },
+  getters: {
+    isLoggedInStatus(state) {
+      return state.isLoggedIn;
+    },
+    productsList(state) {
+      return state.products;
+    },
+    cartItems(state) {
+      return state.cart;
+    },
+  },
+});
+
+export default store;
